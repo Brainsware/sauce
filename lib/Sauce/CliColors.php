8a80bdad320f6387e73a5827dd564ca1fb74a292
Alexander Pánek <a.panek@brainsware.org>
2013-05-10 15:46:15 +0200

    Color codes need a semicolon

--- lib/Sauce/CliColors.php
+++ lib/Sauce/CliColors.php
@@ -24,22 +24,22 @@ namespace Sauce;
 class CliColors
 {
 	public static $foreground_colors = [
-		'black'        => '0,30',
-		'dark_gray'    => '1,30',
-		'blue'         => '0,34',
-		'light_blue'   => '1,34',
-		'green'        => '0,32',
-		'light_green'  => '1,32',
-		'cyan'         => '0,36',
-		'light_cyan'   => '1,36',
-		'red'          => '0,31',
-		'light_red'    => '1,31',
-		'purple'       => '0,35',
-		'light_purple' => '1,35',
-		'brown'        => '0,33',
-		'yellow'       => '1,33',
-		'light_gray'   => '0,37',
-		'white'        => '1,37'
+		'black'        => '0;30',
+		'dark_gray'    => '1;30',
+		'blue'         => '0;34',
+		'light_blue'   => '1;34',
+		'green'        => '0;32',
+		'light_green'  => '1;32',
+		'cyan'         => '0;36',
+		'light_cyan'   => '1;36',
+		'red'          => '0;31',
+		'light_red'    => '1;31',
+		'purple'       => '0;35',
+		'light_purple' => '1;35',
+		'brown'        => '0;33',
+		'yellow'       => '1;33',
+		'light_gray'   => '0;37',
+		'white'        => '1;37'
 	];
 
 	public static $background_colors = [
