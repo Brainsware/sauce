bb4414c3a509868c076232cc2a61104e2e8b339c
Alexander Pánek <a.panek@brainsware.org>
2013-05-13 17:30:32 +0200

    Added first Grease test and dependency

--- Tests/String.php
+++ Tests/String.php
@@ -17,216 +17,46 @@
 
 */
 
-require realpath(__DIR__) . '/../vendor/autoload.php';
+namespace Tests;
 
-/* Should is a class for general asserts and to check whether code throws or
- * does not throw exceptions of given type as expected.
- *
- * TODO: Examples
- */
-class Should
+class String
 {
-	protected $results;
-	protected $name;
+	use \Grease\Test;
 
-	public function __construct ($name, $results = [])
+	public function tests ()
 	{
-		$this->name = $name;
-		$this->results = V($results);
-	}
+		$this->should->throw(
+			'Argument contract __construct',
+			'An InvalidArgumentException should be thrown',
+			'InvalidArgumentException',
+			function () { new \Sauce\String(0); }
+		);
 
-	public function name ()
-	{
-		return $this->name;
-	}
+		$this->should->assert(
+			'String stored internally',
+			'Passing a string to __construct stores that string internally in the object',
+			function () {
+				$string = 'abc';
+				$s = new \Sauce\String($string);
 
-	public function results ()
-	{
-		return $this->results;
-	}
-
-	/* #assert checks whether a given closure executes without throwing an
-	 * exception and returns true.
-	 *
-	 * If the closure is not callable, the test does not pass.
-	 *
-	 * If the closure or any code called therein throws an exception, the test
-	 * does not pass and the exception message and its trace are pushed
-	 * onto the results vector.
-	 *
-	 * TODO: Examples
-	 */
-	public function assert ($name, $description, $fn)
-	{
-		$success = false;
-		$message = '';
-		$trace = V();
-
-		if (!is_callable($fn)) {
-			$success = false;
-			$message = 'Passed test function is not callable';
-
-		} else {
-			try {
-				$success = true === $fn();
-
-			} catch (\Exception $e) {
-				$success = false;
-				$message = "An exception was thrown in file {$e->getFile()}:{$e->getLine()}:\n{$e->getMessage()}";
-				$trace   = V($e->getTrace());
+				return $s->to_s() === $string;
 			}
-		}
+		);
 
-		$this->results->push(A([
-			'name'        => $name,
-			'description' => $description,
-			'success'     => $success,
-			'message'     => $message,
-			'trace'       => $trace
-		]));
-	}
+		$this->should->assert(
+			'String instance stored internally',
+			'Passing a String instance to __construct copies over the stored string',
+			function () {
+				$string = 'abc';
 
-	/* #throw checks whether a given closure executes WITH throwing an
-	 * exception of given type.
-	 *
-	 * If the closure is not callable, the test does not pass.
-	 *
-	 * If the closure or any code called therein throws an exception other
-	 * than the expected, the test does not pass and the exception message
-	 * and its trace are pushed onto the results vector.
-	 *
-	 * TODO: Examples
-	 */
-	public function _throw ($name, $description, $exception_type, $fn)
-	{
-		$success = false;
-		$message = '';
-		$trace   = V();
+				$s1 = new \Sauce\String($string);
+				$s2 = new \sauce\String($s1);
 
-		if (!is_callable($fn)) {
-			$success = false;
-			$message = 'Passed test function is not callable';
-
-		} else {
-			try {
-				$fn();
-
-			} catch (\Exception $e) {
-				if ($exception_type === get_class($e)) {
-					$success = true;
-
-				} else {
-					$type_of_e = get_class($e);
-
-					$message = "An exception other than the expected {$exception_type} was thrown in file {$e->getFile()}:{$e->getLine()}: \n({$type_of_e}) {$e->getMessage()}";
-					$trace   = V($e->getTrace());
-				}
+				return $s2->to_s() === $string;
 			}
-		}
+		);
 
-		$this->results->push(A([
-			'name'        => $name,
-			'description' => $description,
-			'success'     => $success,
-			'message'     => $message,
-			'trace'       => $trace
-		]));
-	}
-
-	/* #not_throw checks whether a given closure executes WITHOUT throwing an
-	 * exception of given type. This method does not check the closure's
-	 * return value.
-	 *
-	 * If the closure is not callable, the test does not pass.
-	 *
-	 * If the closure or any code called therein throws an exception the test
-	 * does not pass and the exception message and its trace are pushed onto
-	 * the results vector.
-	 *
-	 * TODO: Examples
-	 */
-	public function not_throw ($name, $description, $fn, $exception_type)
-	{
-		$success = false;
-		$message = '';
-		$trace   = V();
-
-		if (!is_callable($fn)) {
-			$success = false;
-			$message = 'Passed test function is not callable';
-
-		} else {
-			try {
-				$fn();
-
-				$success = true;
-
-			} catch (\Exception $e) {
-				$type_of_e = get_class($e);
-
-				$message = "An exception other than the expected {$exception_type} was thrown in file {$e->getFile()}:{$e->getLine()}: \n({$type_of_e}) {$e->getMessage()}";
-				$trace   = V($e->getTrace());
-			}
-		}
-
-		$this->results->push(A([
-			'name'        => $name,
-			'description' => $description,
-			'success'     => $success,
-			'message'     => $message,
-			'trace'       => $trace
-		]));
-	}
-
-	/* Since we may not define a method called #throw by hand, we have to walk
-	 * the extra mile and catch it via #__call. */
-	public function __call ($name, $arguments)
-	{
-		switch ($name) {
-			case 'throw': return call_user_func_array([ $this, '_throw' ], $arguments); break;
-
-			default: trigger_error("Call to undefined method ".__CLASS__."::$func()", E_USER_ERROR); break;
-		}
 	}
 }
-
-$should = new Should('String class');
-
-$should->throw('Argument contract __construct', 'An InvalidArgumentException should be thrown',
-	'InvalidArgumentException',
-	function () { new \Sauce\String(0); }
-);
-
-$should->assert('String stored internally', 'Passing a string to __construct stores that string internally in the object',
-	function () {
-		$string = 'abc';
-		$s = new \Sauce\String($string);
-
-		return $s->to_s() === $string;
-	}
-);
-
-$should->assert('String instance stored internally', 'Passing a String instance to __construct copies over the stored string',
-	function () {
-		$string = 'abc';
-
-		$s1 = new \Sauce\String($string);
-		$s2 = new \sauce\String($s1);
-
-		return $s2->to_s() === $string;
-	}
-);
-
-$paint = new \Sauce\CliColors();
-
-printf("%d..%d\n", 1, $should->results()->count());
-printf("# %s:\n", $should->name());
-
-foreach ($should->results()->to_array() as $i => $result) {
-	$success = $result->success ? $paint::green('ok') : $paint::red('not ok');
- 
-	printf("%s %d - %s\n", $success, ($i + 1), $result->name, $success);
-}
-
 
 ?>
