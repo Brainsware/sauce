d01db6468aab6747207d54f7d6c3030b16b4e690
Alexander Pánek <a.panek@brainsware.org>
2013-05-15 16:58:11 +0200

    Added interface tests for Object

---
+++ Tests/Object.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+   Copyright 2012-2013 Brainsware
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+namespace Tests;
+
+class Object
+{
+	use \Grease\Test;
+
+	public function tests ()
+	{
+		$this->interface_tests();
+	}
+
+	protected function interface_tests()
+	{
+		$this->should->implement('Object should implement ArrayAccess', '', 'ArrayAccess', A());
+		$this->should->implement('Object should implement Countable', '', 'Countable', A());
+		$this->should->implement('Object should implement JsonSerializable', '', 'JsonSerializable', A());
+		$this->should->implement('Object should implement Iterator', '', 'Iterator', A());
+	}
+}
