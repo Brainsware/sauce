339ff08948e94d025767311f4e0f7840d8fb9529
Alexander Pánek <a.panek@brainsware.org>
2013-10-25 14:18:08 +0200

    Added first tests for Sauce\Object

--- Tests/Object.php
+++ Tests/Object.php
@@ -28,11 +28,82 @@ class Object
 		$this->interface_tests();
 	}
 
-	protected function interface_tests()
+	protected function interface_tests ()
 	{
 		$this->should->implement('Object should implement ArrayAccess', '', 'ArrayAccess', A());
 		$this->should->implement('Object should implement Countable', '', 'Countable', A());
 		$this->should->implement('Object should implement JsonSerializable', '', 'JsonSerializable', A());
+
+		// NOTE: Not yet implemented, but should be implemented at some point.
 		$this->should->implement('Object should implement Iterator', '', 'Iterator', A());
+
+		$this->should->assert(
+			'Calling count() should always return a numeric value', '',
+			function () {
+				$a = A([
+					'a' => 0,
+					'b' => 1,
+					'c' => 2,
+					'd' => 3
+				]);
+
+				return is_numeric($a->count());
+			}
+		);
+
+		$this->should->assert(
+			'Calling count() should always return a numeric value', '',
+			function () { return is_numeric(A()->count()); }
+		);
+
+		$this->should->assert(
+			'Calling empty() should always return a boolean value', '',
+			function () { return true === A()->is_empty(); }
+		);
+
+		$this->should->assert(
+			'Calling empty() should always return a boolean value', '',
+			function () { return false === A([ 'a' => 1, 'b' => 2, 'c' => 3 ])->is_empty(); }
+		);
+
+		$this->should->assert(
+			'Calling jsonSerialize() should always return an array (the internal storage)', '',
+			function () {
+				$a = A([ 'a' => 1, 'b' => 2, 'c' => 3 ]);
+
+				return is_array($a->jsonSerialize());
+			}
+		);
+
+		$this->should->assert(
+			'Calling jsonSerialize() should always return an array (the internal storage)', '',
+			function () {
+				$a = V();
+
+				return is_array($a->jsonSerialize());
+			}
+		);
+	}
+
+	protected function construct_tests ()
+	{
+		$this->should->assert(
+			'The constructor should take an argument of arbitrary type that is not an array or an instance of \Sauce\Vector and store it in an internal array', '',
+			function () {
+				$a = A(10);
+
+				return $a[0] === 10;
+			}
+		);
+
+		$this->should->assert(
+			'The constructor should take an argument of arbitrary type that is not an array or an instance of \Sauce\Vector and store it in an internal array', '',
+			function () {
+				$a = A('abc');
+
+				return $a[0] === 'abc';
+			}
+		);
+
 	}
 }
